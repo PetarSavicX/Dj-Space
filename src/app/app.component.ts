1ca3b81e89ebd68cddd07dd4e721e64334200e40
Petar Savic <petarsavicx@outlook.com>
2025-04-15 20:05:39 +0200

    small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DeckComponent } from './deck/deck.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DeckComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
