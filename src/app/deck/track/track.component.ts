e207371ea04c79d187e62686284057c93fe075e5
Petar Savic <petarsavicx@outlook.com>
2025-04-15 22:48:02 +0200

    track basic functionality + audio service

--- src/app/deck/track/track.component.ts
+++ src/app/deck/track/track.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { AudioService } from '../../services/audio.service';
 
 @Component({
   selector: 'app-track',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './track.component.html',
   styleUrl: './track.component.css'
 })
-export class TrackComponent {
+export class TrackComponent implements OnInit{
+  @Input() trackLabel!: "A" | "B";
+  audioNo1: string = "assets/audio/beli-free-phd.mp3";
+  audioNo2: string = "assets/audio/beli-free-blingbling.mp3";
+  isLoaded: boolean = false;
 
+  constructor(private audio: AudioService) {}
+
+  ngOnInit(): void {
+  }
+  async load(){
+    this.isLoaded = false;
+    if(this.trackLabel == 'A'){
+      try {
+        await this.audio.loadTrackByUrl(this.trackLabel, this.audioNo1);
+        console.log("Ucitana pesma A");
+      } catch (err){
+        console.error("Greska sa A")
+      }
+    }
+    if(this.trackLabel == 'B'){
+      try {
+        await this.audio.loadTrackByUrl(this.trackLabel, this.audioNo2);
+        console.log("Ucitana pesma B");
+      } catch (err){
+        console.error("Greska sa B")
+      }
+    }
+    this.isLoaded = true;
+  }
+
+  play(){
+    this.audio.play(this.trackLabel);
+  }
+
+  pause(){
+    this.audio.pause(this.trackLabel);
+  }
 }
---
+++ src/app/services/audio.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+
+interface TrackData {
+  buffer: AudioBuffer | null;
+  sourceNode: AudioBufferSourceNode | null;
+  gainNode: GainNode,
+  startTime: number | null,
+  pauseOffset: number | null
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AudioService {
+  private audioContext: AudioContext = new AudioContext();
+  private tracks: { [trackId: string]: TrackData} = {}
+
+  constructor() { }
+
+  async loadTrackByUrl(trackId: string, url: string): Promise<void> {
+    const response = await fetch(url);
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = await this.audioContext.decodeAudioData(arrayBuffer);
+
+    if(!this.tracks[trackId]){
+      const gainNode = this.audioContext.createGain();
+      gainNode.connect(this.audioContext.destination)
+      this.tracks[trackId] = {
+        buffer: buffer,
+        sourceNode: null,
+        gainNode: gainNode,
+        startTime: null,
+        pauseOffset: null
+      }
+    }
+    else this.tracks[trackId].buffer = buffer;
+  }
+
+  play(trackId: string): void {
+    const track = this.tracks[trackId];
+
+    if (!track || !track.buffer) {
+      console.warn(`Track '${trackId}' nije spreman.`);
+      return;
+    }
+
+    if (this.audioContext.state === 'suspended') {
+      this.audioContext.resume();
+    }
+
+    const sourceNode = this.audioContext.createBufferSource();
+    sourceNode.buffer = track.buffer;
+    sourceNode.connect(track.gainNode);
+    if(!track.pauseOffset) sourceNode.start(0, 0);
+    else sourceNode.start(0, track.pauseOffset);
+    track.sourceNode = sourceNode;
+    track.startTime = this.audioContext.currentTime - (track.pauseOffset || 0);
+  }
+
+  pause(trackId: string){
+    const track = this.tracks[trackId];
+    const offset = track.startTime ? this.audioContext.currentTime - track.startTime : 0;
+    track.pauseOffset = offset;
+
+    console.log(`Start: ${track.startTime}\nPause: ${track.pauseOffset}`);
+
+    if (!track || !track.buffer) {
+      console.warn(`Track '${trackId}' nije spreman.`);
+      return;
+    }
+
+    track.sourceNode?.stop();
+  }
+}
